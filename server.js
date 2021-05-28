ebe4bf5fd80088f15d9e00fa0febe25fd394371b
Forresterjamie01 <Forresterjamie01@yahoo.com>
2021-05-28 14:28:39 -0400

    skeletons routes passed tests working and operating.

--- server.js
+++ server.js
@@ -15,6 +15,36 @@ if (process.env.NODE_ENV === "production") {
 app.get("/api/user", function (req, res){
   res.json([])
 })
+
+app.get("/api/user/:id", function (req, res){
+  res.json([])
+})
+
+app.post("/api/signup", function (req, res){
+  res.json([])
+})
+
+app.post("/api/login", function (req, res){
+  res.json([])
+})
+
+app.get("/api/users/collaborator?pm=true&me=true&m=true&vp=true&location=<nameofpalce>”", function (req, res){
+  res.json([])
+})
+
+app.post("/api/connect", function (req, res){
+  res.json([])
+})
+
+app.post("/api/user/:id/connect/:connectID", function (req, res){
+  res.json([])
+})
+
+app.get("/api/user/:id/connections", function (req, res){
+  res.json([])
+})
+
+
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
